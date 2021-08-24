a3fb9d6f73a93b66a0a74068951791389f0b189f
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:55:24 -0300

    criacao da pagina com sabor de frutas vermelhas

---
+++ pages/Frutasvermelhas.php
@@ -0,0 +1,18 @@
+<html>
+
+ 
+  <h2>Donuts Frutas Vermelhas</h2>
+
+<div class="main">
+  <aside class="texto">
+  <?php echo $produtos["frutasvermelhas"]["descricao"]?>
+   </aside>
+
+   <div>
+     <img class="img-prod" src="imagens/ringdonuts.png" alt="Frutas Vermelhas">
+     <div class="botao-valor">R$<?php echo $produtos["frutasvermelhas"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+   </div>
+</div>
+
+</html>
