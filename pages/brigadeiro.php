75214dc09d176c3abf936fc7c8f0c0b0a8cb1532
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:59:58 -0300

    criacao da pagina do donut sabor brigadeiro

---
+++ pages/brigadeiro.php
@@ -0,0 +1,20 @@
+<html>
+<body>
+ 
+  <h2>Brigadeiro</h2>
+
+
+<div class="main">
+  <aside class="texto">
+  <?php echo $produtos["brigadeiro"]["descricao"]?>
+   </aside>
+
+   <div>
+     <img class="img-prod" src="imagens/brigadeiro.png"  alt="ringbrigadeiro">
+     <div class="botao-valor">R$<?php echo $produtos["brigadeiro"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+   </div>
+</div>
+
+</body>
+</html>
