a48efda5470da5f7c9eaf2e02766b2377c8bcb1b
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:50:59 -0300

    criacao da pagina dados

---
+++ dados/dados.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Happy Donut</title>
+</head>
+<body>
+ 
+</body>
+</html>
+
+<?php
+
+$produtos = [
+
+  'tradicional'=>[
+    'descricao'=>"Nossos Delicios Donuts Tradicionais, cobertos com açucar refinado.",
+    'preco'=> 4.99,
+  ],
+
+  "frutasvermelhas" =>[
+    "descricao"=>"Nossos Delicios Donuts de Frutas Vermelhas, com cobertura de morango e recheio de cereja.",
+    'preco'=>7.99,
+  ],
+
+  'fantasy'=>[
+    'descricao'=>"Nossos Delicios Donuts Fantasy, viva a fantasia de saborear um delicioso donut com cobertura de laranja e recheio de morango.",
+    'preco'=> 7.99,
+  ],
+
+  'brigadeiro'=>[
+    'descricao'=>"Nossos Delicios Donuts de Brigadeiro, uma saborosa cobertura de brigadeiro com recheio de beijinho.",
+    'preco'=> 7.99,
+  ], 
+
+];
+  
+ ?>
