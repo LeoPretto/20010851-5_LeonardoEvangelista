037f177fe3b09e1b25cc62ab4ff7d338cda5389b
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:49:06 -0300

    criando pagina de localizacao

---
+++ pages/localização.php
@@ -0,0 +1,15 @@
+
+<html>
+<body>
+
+<h2>Nossa localização</h2>
+<div id="map-container-google-1" class="z-depth-1-half map-container" style="height: 500px">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3887.495422943224!2d-38.53516254953766!3d-13.004092190789608!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71603846df16a97%3A0xae4d3a65f02a2987!2sLargo%20do%20P%C3%B4rto%20da%20Barra%2C%203722-3820%20-%20Barra%2C%20Salvador%20-%20BA%2C%2040130-180!5e0!3m2!1spt-BR!2sbr!4v1629734546741!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
+
+
+  
+</body>
+</html>
+
+
