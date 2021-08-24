25701e8e948b96aa634ad9d815413239b58beed1
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:58:37 -0300

    Criacao da pagina sabor fantasy

---
+++ pages/Fantasy.php
@@ -0,0 +1,20 @@
+<html> 
+<body> 
+
+<h2>Donuts Fantasy</h2>
+
+
+<div class="main">
+  <aside class="texto">
+  <?php echo $produtos["fantasy"]["descricao"]?>
+   </aside>
+
+   <div>
+     <img class="img-prod" src="imagens/donutfantasy.png"  alt="imagem_de_bolos">
+     <div class="botao-valor">R$<?php echo $produtos["fantasy"]["preco"]?></div>
+     <div class="botao-valor"><button  class="comprar">Comprar</button></div>
+   </div>
+</div>
+
+</body>
+</html>
