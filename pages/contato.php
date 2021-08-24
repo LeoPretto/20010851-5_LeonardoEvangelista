43d9fb95ada4da90eb89b81dc4b2809cfedd662a
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:45:48 -0300

    Criacao da pagina contato

---
+++ pages/contato.php
@@ -0,0 +1,19 @@
+<div class="container-contatos">
+<h2>Contatos</h2>
+<div class="cont">
+
+<aside>
+<p> Entre Em Contato conosco para dicas e mais sugestões de sabores, ficaremos felizes em receber sua mensagem.</p>
+</aside>
+<div class="contatos">
+<label for="">Seu nome:</label>
+<input type="text" name="nome" placeholder="..." id="nome">
+
+<label for="email">Digite seu e-mail:</label>
+<input type="email" id="email" name="email">
+
+<label for="">Deixe sua mensagem:</label>
+<textarea name="" id="" cols="30" rows="8"></textarea>
+</div>
+</div>
+</div>
