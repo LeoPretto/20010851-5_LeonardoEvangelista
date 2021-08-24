fea0e06ce52a32e08a8ce75e04a2c13f80396391
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 19:47:19 -0300

    criacao da pagina quem somos

---
+++ pages/quemsomos.php
@@ -0,0 +1,14 @@
+
+<h2>Quem Somos</h2>
+<div class="main">
+<p>      
+    Sonho do casal de imigrantes espanhóis, que já não viam mais futuro na sua freguesia de salgados
+    feitos no food-truck do Porto da Barra-Salvador. Procuraram então, uma alternativa de negócio.
+    Depois de muita ajuda e esforço, em Janeiro de 2010, fundaram uma fabrica de rosquinhas em casa
+    o casal e quatro filhos, foram firmando seu espaço.
+    Sempre preocupados em servir bem, traziam sempre novidades e pedidos dos clientes.
+    Pouco a pouco a Doces Sonhos foi ganhando a clientela da vizinhança e recuperando antigos
+    clientes que antes consumiam seus salgados.
+</p>
+<img src="./imagens/logo.png" alt="logo-HappyDonut" style="width: 340px;">
+</div>
