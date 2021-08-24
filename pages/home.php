a80a4aafc9287763e7de1729cf22810a26fe8307
LeoPretto <ra20010851-5@aluno.unicesumar.edu.br>
2021-08-24 20:02:09 -0300

    criacao da pagina home

---
+++ pages/home.php
@@ -0,0 +1,39 @@
+<h2>Nossos Produtos</h2>
+
+<div class="produtos">
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/donutt.png" alt="Donut Tradicional">
+  <div class="card-body">
+    <h5 class="card-title"> Donuts Tradicional </h5>
+    <p class="card-text"> Donuts Tradicional </p>
+    <a href="./index.php?p=tradicional" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/ringdonuts.png" alt="Donut Frutas Vermelhas">
+  <div class="card-body">
+    <h5 class="card-title"> Donuts Frutas Vermelhas </h5>
+    <p class="card-text"> Donuts Recheado Frutas Vermelhas</p>
+    <a href="./index.php?p=frutasvermelhas"  class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/donutfantasy.png" alt="Donuts Fantasy">
+  <div class="card-body">
+    <h5 class="card-title">Donuts Fantasy</h5>
+    <p class="card-text"> Ring Donuts Fantasy</p>
+    <a href="./index.php?p=Fantasy" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+
+<div class="card" style="width: 15rem;">
+  <img class="card-img-top" src="./imagens/brigadeiro.png" alt="donutbrigadeiro">
+  <div class="card-body">
+    <h5 class="card-title">Donuts Brigadeiro</h5>
+    <p class="card-text"> Ring Donuts Brigadeiro </p>
+    <a href="./index.php?p=brigadeiro" class="btn btn-primary">Saiba mais</a>
+  </div>
+</div>
+</div>
